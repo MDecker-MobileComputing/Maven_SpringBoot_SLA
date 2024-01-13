5d66d4a91f7ed27e8a58f501387221fee3f780c9
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-01-13 17:50:37 +0100

    dazu: ThymeleafViewcontroller mit leerer Methode slaWertUmrechnen()

---
+++ src/main/java/de/eldecker/dhbw/spring/sla/thymeleaf/ThymeleafViewController.java
@@ -0,0 +1,39 @@
+package de.eldecker.dhbw.spring.sla.thymeleaf;
+
+import de.eldecker.dhbw.spring.sla.logik.SLARechner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+/**
+ * Controller für Thymeleaf-Templates.
+ */
+@Controller
+public class ThymeleafViewController {
+
+    /** Bean mit Business-Logik für SLA-Berechnung.  */
+    private SLARechner _slaRechner;
+
+
+    /**
+     * Konstruktor für Dependency Injection.
+     */
+    @Autowired
+    public ThymeleafViewController(SLARechner slaRechner) {
+
+        _slaRechner = slaRechner;
+    }
+
+
+    @GetMapping("/sla/{slaWert}")
+    public String slaWertUmrechnen(@PathVariable String slaWertProzent,
+                                   Model model) {
+
+        return "";
+    }
+
+}
