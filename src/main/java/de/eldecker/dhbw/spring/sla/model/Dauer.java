fd06bd8c9c3cd93124fd5208a428cbda6a00354a
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-01-13 16:07:55 +0100

    Neu: DauerTest

--- src/main/java/de/eldecker/dhbw/spring/sla/model/Dauer.java
+++ src/main/java/de/eldecker/dhbw/spring/sla/model/Dauer.java
@@ -12,4 +12,29 @@ public record Dauer( int wochen,
                      int tage,
                      int stunden,
                      int minuten,
-                     int sekunden ) {}
+                     int sekunden ) {
+    
+    /**
+     * Liefer gut lesbare String-Repräsentation der Dauer zurück.
+     * 
+     * @return Zeitdauer, z.B. "2 Wochen, 1 Tag, 3 Stunden, 1 Minute und 33 Sekunden"
+     */
+    @Override
+    public String toString() {
+        
+        // Einheit in Plural oder Singular
+        final String einheitWochen   = wochen   == 1 ? "Woche"   : "Wochen"; 
+        final String einheitTage     = tage     == 1 ? "Tag"     : "Tage";
+        final String einheitStunden  = stunden  == 1 ? "Stunde"  : "Stunden";
+        final String einheitMinuten  = minuten  == 1 ? "Minute"  : "Minuten";
+        final String einheitSekunden = sekunden == 1 ? "Sekunde" : "Sekunden";
+        
+        return String.format( "%d %s, %d %s, %d %s, %d %s und %d %s", 
+                              wochen   , einheitWochen,
+                              tage    , einheitTage,
+                              stunden , einheitStunden,
+                              minuten , einheitMinuten,
+                              sekunden, einheitSekunden );
+    }
+    
+}
