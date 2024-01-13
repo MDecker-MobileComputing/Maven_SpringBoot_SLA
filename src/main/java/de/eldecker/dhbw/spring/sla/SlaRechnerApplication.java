6634267dadabd0162dd0663063b8e033c1a1eeaf
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-01-13 11:49:50 +0100

    Initiales Projekt

---
+++ src/main/java/de/eldecker/dhbw/spring/sla/SlaRechnerApplication.java
@@ -0,0 +1,15 @@
+package de.eldecker.dhbw.spring.sla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SlaRechnerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SlaRechnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/eldecker/dhbw/spring/sla/SlaRechnerApplicationTests.java
@@ -0,0 +1,14 @@
+package de.eldecker.dhbw.spring.sla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SlaRechnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
